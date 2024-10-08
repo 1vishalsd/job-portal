8b8e9911f947585251a3e38a5776e9d9ed7b0d6d
1VishalSD <ilearnvk@gmail.com>
2024-10-08 09:38:29 +0530

    setup the server

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,19 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 const app = express();
+
+// middlewares
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+const corsOptions = {
+  origin: "http//localhost:5173",
+  credentials: true,
+};
+app.use(cors(corsOptions));
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running at Port ${PORT}`);
